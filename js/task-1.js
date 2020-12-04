466fe981fb4c5df2c039840b9393e99fa4f92d6b
vladho <vladhopilo@gmail.com>
2020-12-05 01:38:46 +0200

    upd

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const name = "Генератор защитного поля";
+let price = 1000;
+
+// let phrase = "Выбран " + name + ", цена за штуку " + price + " кредитов";
+console.log("Выбран " + name + ", цена за штуку " + price + " кредитов");
+
+price = 2000;
+console.log("Выбран " + name + ", цена за штуку " + price + " кредитов");
